bf52eacbf9f12dbfe670beb0f0fe2bb95d20cd94
Mariana Alvarez <mariana.alvarez@concrete.com.br>
2019-09-04 01:42:58 -0300

    Create CarRepairTableViewDataProvider

---
+++ CarRepair/CarRepair/Features/Search/DataProvider/CarRepairTableViewDataProvider.swift
@@ -0,0 +1,43 @@
+//
+//  CarRepairTableViewDataProvider.swift
+//  CarRepair
+//
+//  Created by mariana.alvarez on 9/4/19.
+//  Copyright © 2019 mariana.alvarez. All rights reserved.
+//
+
+import UIKit
+
+final class CarRepairTableViewDataProvider: NSObject, UITableViewDataSource, UITableViewDelegate {
+    private var carRepairList = [CarRepair]()
+
+    override init() {}
+
+    // MARK: UITableViewDataSource conforms
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return carRepairList.count
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: CarRepairViewCell.reuseIdentifier) as? CarRepairViewCell {
+            let item = carRepairList[indexPath.row]
+            let viewModel = CarRepairViewModel(carRepair: item)
+            cell.setup(viewModel: viewModel)
+
+            return cell
+        }
+        return UITableViewCell()
+    }
+
+    // MARK: UITableViewDelegate conforms
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 160
+    }
+}
+
